0d7a5b581852da3c2b10c00e0c8870623b32f152
Fántics Attila <fantics.attila@windowslive.com>
2018-11-03 17:10:02 +0100

    Products Index oldal elkészítése

---
+++ Granite_House/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Granite_House.Data;
+using Granite_House.Models;
+using Granite_House.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Granite_House.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ProductsViewModel ProductsVM { get; set; }
+
+        public ProductsController(ApplicationDbContext db)
+        {
+            _db = db;
+            ProductsVM = new ProductsViewModel()
+            {
+                ProductTypes = _db.ProductTypes.ToList(),
+                SpecialTags = _db.SpecialTags.ToList(),
+                Products = new Products()
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = _db.Products.Include(pt => pt.ProductTypes).Include(st => st.SpecialTags);
+            return View(await products.ToListAsync());
+        }
+    }
+}
