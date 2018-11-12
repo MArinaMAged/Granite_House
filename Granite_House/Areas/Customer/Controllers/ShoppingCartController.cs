0ddb4ec57b8ddb642dea484815284c40a5658e31
Fántics Attila <fantics.attila@windowslive.com>
2018-11-12 19:13:24 +0100

    Shopping Cart - Index felület elkészítése + Date/Time Picker használata

---
+++ Granite_House/Areas/Customer/Controllers/ShoppingCartController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Granite_House.Data;
+using Granite_House.Extensions;
+using Granite_House.Models;
+using Granite_House.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Granite_House.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class ShoppingCartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
+        public ShoppingCartViewModel ShoppingCartVM { get; set; }
+
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            _db = db;
+            ShoppingCartVM = new ShoppingCartViewModel()
+            {
+                Products = new List<Models.Products>()
+            };
+        }
+
+        //GET Index Shopping Cart
+        public IActionResult Index()
+        {
+            List<int> lstShoppingCart = HttpContext.Session.Get<List<int>>("ssShoppingCart");
+            if(lstShoppingCart!=null && lstShoppingCart.Count>0)
+            {
+                foreach(int cartItem in lstShoppingCart)
+                {
+                    Products products = _db.Products
+                        .Include(p=>p.SpecialTags)
+                        .Include(p=>p.ProductTypes)
+                        .Where(p => p.Id == cartItem).FirstOrDefault();
+                    ShoppingCartVM.Products.Add(products);
+                }
+            }
+            return View(ShoppingCartVM);
+        }
+    }
+}
