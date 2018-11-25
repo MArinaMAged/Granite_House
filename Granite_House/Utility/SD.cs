f879ce01114ade1b8b006b0d13978aff28e3cf0f
Fántics Attila <fantics.attila@windowslive.com>
2018-11-25 14:58:08 +0100

    Register Post módosítása (Név, Email, Jogosultság mentése)

--- Granite_House/Utility/SD.cs
+++ Granite_House/Utility/SD.cs
@@ -4,5 +4,8 @@
     {
         public const string DefaultProductImage = "No-image-available.jpg";
         public const string ImageFolder = @"images\ProductImage";
+
+        public const string AdminEndUser = "Admin";
+        public const string SuperAdminEndUser = "SuperAdmin";
     }
 }
