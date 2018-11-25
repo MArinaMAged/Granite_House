3178085ef27499f2f5c872585d08d0d3147724b7
Fántics Attila <fantics.attila@windowslive.com>
2018-11-25 09:35:27 +0100

    Identity oldalak berakása a projektbe
    Project->Add new Scaffolded Item->Identity-Add

---
+++ Granite_House/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Granite_House.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Granite_House.Areas.Identity.IdentityHostingStartup))]
+namespace Granite_House.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
