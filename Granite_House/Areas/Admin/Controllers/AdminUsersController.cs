f152cf9c7c40f0c3294cec65e8b80b5021e2fd81
Fántics Attila <fantics.attila@windowslive.com>
2018-11-25 18:09:09 +0100

    Admin Users - Edit

--- Granite_House/Areas/Admin/Controllers/AdminUsersController.cs
+++ Granite_House/Areas/Admin/Controllers/AdminUsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Granite_House.Data;
+using Granite_House.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Granite_House.Areas.Admin.Controllers
@@ -19,6 +20,38 @@ namespace Granite_House.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View(_db.ApplicationUsers.ToList());
+        }
+
+        //Get Edit
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || id.Trim().Length == 0)
+                return NotFound();
+
+            var userFromDb = await _db.ApplicationUsers.FindAsync(id);
+            if (userFromDb == null)
+                return NotFound();
+            return View(userFromDb);
+        }
+
+        //Post Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(string id, ApplicationUser applicationUser)
+        {
+            if (id != applicationUser.Id)
+                return NotFound();
+            if(ModelState.IsValid)
+            {
+                ApplicationUser userFromDb = _db.ApplicationUsers.Where(p => p.Id == id).FirstOrDefault();
+                userFromDb.Name = applicationUser.Name;
+                userFromDb.PhoneNumber = applicationUser.PhoneNumber;
+
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(applicationUser);
         }
     }
 }
