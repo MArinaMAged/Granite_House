9583ddb2424c118f8525895ceb9602c3ce0b7986
Fántics Attila <fantics.attila@windowslive.com>
2018-11-27 19:52:44 +0100

    View Components - Hello <felhasználó>!
    A Default.cshtml mappáját kézzel kellett létrehozni!

---
+++ Granite_House/ViewComponents/UserNameViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Granite_House.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Granite_House.ViewComponents
+{
+    public class UserNameViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserNameViewComponent(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public object ClaimIdentity { get; private set; }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claims = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var userFromDb = await _db.ApplicationUsers.Where(p => p.Id == claims.Value).FirstOrDefaultAsync();
+
+            return View(userFromDb);
+        }
+    }
+}
